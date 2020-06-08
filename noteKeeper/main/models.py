82ba4a8e6f542d91874aa1ac213b2fec9050ea0b
Anjani100 <anjani10.kumar@gmail.com>
2020-06-08 12:11:06 +0530

    Department model uploaded.

--- noteKeeper/main/models.py
+++ noteKeeper/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class department(models.Model):
+	department_name = models.CharField(max_length = 200)
+	department_slug = models.CharField(max_length = 200, default = 1)
+
+	def __str__(self):
+		return self.department_name
--- noteKeeper/main/views.py
+++ noteKeeper/main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import department
 
-# Create your views here.
+
+def homepage(request):
+	return render(request = request,
+				 template_name = "main/home.html",
+				 context = {"department": department.objects.all})
