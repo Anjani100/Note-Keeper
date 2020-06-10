8bbf4ab443d5d4006289cbd71da781af31023bd4
Anjani100 <anjani10.kumar@gmail.com>
2020-06-10 11:03:38 +0530

    register page modified

--- noteKeeper/noteKeeper/settings.py
+++ noteKeeper/noteKeeper/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -51,6 +52,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'noteKeeper.urls'
+CRISPY_TEMPLATE_PACK="bootstrap4"
 
 TEMPLATES = [
     {
