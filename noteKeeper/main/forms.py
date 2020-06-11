b4ecbc99f9c9d21591c89f99cb268f9b62873d4a
Anjani100 <42367562+Anjani100@users.noreply.github.com>
2020-06-11 15:00:44 +0530

    Register page modified.

--- noteKeeper/main/forms.py
+++ noteKeeper/main/forms.py
@@ -3,24 +3,26 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 
 class RegistrationForm(UserCreationForm):
-	email = forms.EmailField(required = True)
-	college = forms.CharField(max_length = 200, required = True)
+	username = forms.CharField(label = 'Username', max_length = 150)
+	email = forms.EmailField(label = 'Email', required = True)
+	college = forms.CharField(label = 'College', max_length = 200)
+	password1 = forms.CharField(label = 'Password', max_length = 32, widget = forms.PasswordInput)
+	password2 = forms.CharField(label = 'Confirm Password', max_length = 32, widget = forms.PasswordInput)
 
 	class Meta:
 		model = User
-		fields = ('username', 'email', 'college', 'password1', 'password2')
+		fields = ('username', 'college', 'email', 'password1', 'password2')
 
 	def save(self, commit=True):
 		user = super(RegistrationForm, self).save(commit=False)
 		user.email = self.cleaned_data["email"]
-		user.college = self.cleaned_data["college"]
 		if commit:
 			user.save()
 		return user
 
 class LoginForm(AuthenticationForm):
-	email = forms.EmailField(required = False)
+	# email = forms.EmailField(required = False)
 
 	class Meta:
 		model = User
-		fields = ('username', 'email', 'password')
+		fields = ('username', 'password')
--- noteKeeper/main/views.py
+++ noteKeeper/main/views.py
@@ -5,6 +5,13 @@ from django.contrib.auth import logout, authenticate, login
 from django.contrib import messages
 from .forms import RegistrationForm, LoginForm
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+
+def account(request, user):
+	user = User.objects.get(username = username)
+	return render(request,
+				 template_name = "main/account.html",
+				 context = {"user": user})
 
 def homepage(request):
 	return render(request = request,
@@ -12,26 +19,26 @@ def homepage(request):
 				 context = {"department": department.objects.all})
 
 def register(request):
-    if request.method == "POST":
-        form = RegistrationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            username = form.cleaned_data.get('username')
-            messages.success(request, f"New Account Created: {username}")
-            login(request, user)
-            messages.info(request, f"You are now logged in as {username}")
-            return redirect("main:homepage")
+	if request.method == "POST":
+		form = RegistrationForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			username = form.cleaned_data.get('username')
+			messages.success(request, f"New Account Created: {username}")
+			login(request, user)
+			messages.info(request, f"You are now logged in as {username}")
+			return redirect("main:homepage")
 
-        else:
-            for msg in form.error_messages:
-            	messages.error(request, f"{msg}: {form.error_messages[msg]}")
+		else:
+			for msg in form.error_messages:
+				messages.error(request, f"{msg}: {form.error_messages[msg]}")
 
-            return render(request = request,
+			return render(request = request,
                           template_name = "main/register.html",
                           context={"form":form})
 
-    form = RegistrationForm
-    return render(request = request,
+	form = RegistrationForm
+	return render(request = request,
                   template_name = "main/register.html",
                   context={"form":form})
 
